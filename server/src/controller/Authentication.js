155ea684f29da54b1e387ca3c1bda2ea34b11575
Lucas da Silva Souto <lssouto96@gmail.com>
2018-03-08 13:27:20 -0300

    add jsonWebToken

--- server/src/controller/Authentication.js
+++ server/src/controller/Authentication.js
@@ -1,26 +1,29 @@
 import UserServ from '../service/user'
 import Model from '../model'
-// import jwt from 'jsonwebtoken'
-// import config from '../config'
+import jwt from 'jsonwebtoken'
+import config from '../config'
 
-// function jwtSignUser (user){
-//     const ONE_WEEK = 60*60*24*7
-//     return jwt.sign(user, config.data.auth.jwtSecret,{
-//         expiresIn:ONE_WEEK
-//     })
-// }
+function jwtSignUser (user){
+    const ONE_DAY = 60*60*24
+    //let jsonUser = JSON.parse(JSON.stringify(user)) // Need of a pure object -_- really ?
+    return jwt.sign(user, config.data.auth.jwtSecret,{
+        expiresIn:ONE_DAY
+    })
+}
 
 export default {
     verify: (req,res)=>{
         try {
-            UserServ.getOne(Model.userModel, req.body.username,(user)=>{
+            UserServ.getOne(Model.userModel, req.body.username, async (user)=>{
                 if(user.password === req.body.pwd){
-
                     res.send({
                         data: user.username,
                         status: true,
-                        msg: 'Login Realizado'
-                        //token: jwtSignUser(user)
+                        msg: 'Login Realizado',
+                        token: await jwtSignUser({
+                            username: user.username,
+                            id : user._id 
+                        })
                     })
                 }
                 else{
